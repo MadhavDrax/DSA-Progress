89177d46b205cf842e76a821b9442644903f9892
Madhav Ojha <93091299+MadhavDrax@users.noreply.github.com>
2023-03-06 14:53:42 +0530

    Add files via upload

---
+++ HalfPiramid.java
@@ -0,0 +1,16 @@
+/* Pattern 3
+*
+**
+***
+**** 
+ */
+public class HalfPiramid {
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
